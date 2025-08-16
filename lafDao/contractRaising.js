3d017312420854e0f2ff2549d8374e3bd91b5982
testydj <test_ydj@outlook.com>
2025-08-16 16:59:02 +0800

    init

---
+++ lafDao/contractRaising.js
@@ -0,0 +1,368 @@
+
+/*global ethereum, MetamaskOnboarding */
+
+
+const forwarderOrigin = 'http://localhost:9011'
+
+
+
+
+/**
+ * 添加网络方法
+ * 参数 params
+ * 参数示例
+ * [
+    {
+      chainId: '0x38',
+      chainName: 'BSC',
+      nativeCurrency: {
+        name: 'BNB',
+        symbol: 'BNB',
+        decimals: 18,
+      },
+      rpcUrls: ['https://bsc-dataseed.binance.org/'],
+      blockExplorerUrls: ['https://bscscan.com/'],
+    },
+  ]
+*/
+function wallet_addEthereumChain(params) {
+  window.ethereum &&
+    window.ethereum
+      .request({
+        method: 'wallet_addEthereumChain',
+        params: params,
+      })
+      .then(() => {
+        console.log('添加成功')
+      })
+      .catch((e) => {
+
+      })
+}
+
+$('#wallet_addEthereumChain').on('click', function() {
+  console.log('wallet_addEthereumChain');
+  var params = [
+    {
+      chainId: '0x38',
+      chainName: 'BSC',
+      nativeCurrency: {
+        name: 'BNB',
+        symbol: 'BNB',
+        decimals: 18,
+      },
+      rpcUrls: ['https://bsc-dataseed.binance.org/'],
+      blockExplorerUrls: ['https://bscscan.com/'],
+    },
+  ];
+  wallet_addEthereumChain(params);
+})
+
+/**
+ * 切换网络
+ * 参数params
+ * 参数示例
+ * [
+    {
+      "chainId": "0x64"
+    },
+  ]
+*/
+function wallet_switchEthereumChain(params) {
+  window.ethereum &&
+    window.ethereum
+      .request({
+        method: 'wallet_switchEthereumChain',
+        params: params,
+      })
+      .then(() => {
+        console.log('切换成功')
+      })
+      .catch((e) => {
+
+      })
+}
+
+$('#wallet_switchEthereumChain').on('click', function() {
+  console.log('wallet_switchEthereumChain');
+  var params = [
+    {
+      "chainId": "0x64"
+    },
+  ];
+  wallet_switchEthereumChain(params);
+})
+
+
+/**
+ * 请求权限
+ * 参数params
+ * 参数示例
+ * [
+    {
+      "chainId": "0x64"
+    },
+  ]
+*/
+function wallet_requestPermissions(params) {
+  window.ethereum &&
+    window.ethereum
+      .request({
+        method: 'wallet_requestPermissions',
+        params: params,
+      })
+      .then(() => {
+        console.log('请求权限')
+      })
+      .catch((e) => {
+
+      })
+}
+
+$('#wallet_requestPermissions').on('click', function() {
+  console.log('wallet_requestPermissions');
+  var params = [
+    {
+      "eth_accounts": {}
+    },
+  ];
+  wallet_requestPermissions(params);
+})
+
+
+/**
+ * 获取权限(帐号、钱包地址)
+ * 参数params 可为空
+ * 参数示例
+*/
+function wallet_getPermissions(params) {
+  window.ethereum &&
+    window.ethereum
+      .request({
+        method: 'wallet_getPermissions',
+        params: params,
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch((e) => {
+
+      })
+}
+
+$('#wallet_getPermissions').on('click', function() {
+  console.log('wallet_getPermissions');
+  var params = [];
+  wallet_getPermissions(params);
+})
+
+/**
+ * 获取钱包地址列表(帐号、钱包地址)--
+ * 参数params 可为空
+ * 参数示例
+*/
+function eth_accounts(params) {
+
+  const data = null;
+  window.ethereum &&
+    window.ethereum
+      .request({
+        method: 'eth_accounts',
+        params: params,
+      })
+      .then(res => {
+        console.log(res)
+        
+      })
+      .catch((e) => {
+
+      })
+}
+
+$('#eth_accounts').on('click', function() {
+  console.log('eth_accounts');
+  var params = [];
+  eth_accounts(params);
+})
+
+let accounts = "";
+web3.eth.getAccounts().then(res => {
+  accounts = res[0];
+})
+
+
+
+var lafDaoAddress = "0x7C79Bb4F8A4173Ce62AbbC285FaB96358CC02Eb5";
+var raisingAddress = "0x6E04478eA898c24215Ad5Bc6a0F5DB99C5a2deb5";
+var BURN_ADDRESS = '0x0000000000000000000000000000000000000000';
+var USDT_ADDRESS = "0x48afC9801eB27281C3a9d35f34cF15E8533bb87e";
+
+let usdtContract = new web3.eth.Contract(erc20ABI, USDT_ADDRESS);
+let lafDaoContract = new web3.eth.Contract(erc20ABI, lafDaoAddress);
+let raisingContract = new web3.eth.Contract(raisingABI, raisingAddress);
+let participatingAmount = [100,200,300];
+
+  
+///-------
+  //最大募集
+  raisingContract.methods.totalSupplyMax().call(null, function (error, data) {
+    console.log(data);
+    $('#totalSupplyMax').html(web3.utils.fromWei(data, "ether"))
+  });
+
+  //已募集额度
+  raisingContract.methods.totalSupply().call(null, function (error, data) {
+    console.log(data);
+    $('#totalSupply').html(web3.utils.fromWei(data, "ether"))
+  });
+  
+
+  //私募额
+  raisingContract.methods.participatingAccountAmount(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#participatingAccountAmount').html(web3.utils.fromWei(data, "ether"))
+  });
+
+
+
+  //代币余额
+  raisingContract.methods.balanceOf(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#balanceOf').html(web3.utils.fromWei(data, "ether"))
+  });
+
+
+  //奖励额
+  raisingContract.methods.awardsToken(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#awardsToken').html(web3.utils.fromWei(data, "ether"))
+  });
+
+
+  //是否绑定邀请人
+  raisingContract.methods.isBindReferral(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#isBindReferral').html(data)
+  });
+
+
+  
+  //查询邀请人地址
+  raisingContract.methods.getReferral(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#getReferral').html(data)
+  });
+
+
+  //推荐人数查询
+  raisingContract.methods.getReferralCount(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#getReferralCount').html(data)
+  });
+
+
+  //推荐列表
+  raisingContract.methods.getReferralsList(accounts).call(null, function (error, data) {
+    console.log(data);
+    $('#getReferralsList').html(data)
+  });
+
+
+  //向上查找邀请人链路
+  raisingContract.methods.getReferrals(accounts, 30).call(null, function (error, data) {
+    console.log(data);
+    $('#getReferrals').html(data)
+  });
+
+
+  //绑定邀请人
+$('#setReferrer').on('click', async function() {
+  var referrerAddress = $("#referrerAddress").val();
+  raisingContract.methods.bindReferral(referrerAddress).send({from: accounts}, function (error, transactionHash) {
+    console.log("setReferrer>>>>>", error, transactionHash);
+  });
+})
+
+
+//参与
+$('#participating').on('click', async function() {
+  var participatingIndex = $("#participatingIndex").val();
+  var usdtAmount = participatingAmount[participatingIndex];
+  var inUsdt = web3.utils.toWei(usdtAmount, "ether");
+  usdtContract.methods.approve(raisingAddress, inUsdt).send({ from: accounts }).then((approveHash) => {
+    raisingContract.methods.participating(participatingIndex).send({from: accounts}, function (error, transactionHash) {
+      console.log("participating>>>>>", error, transactionHash);
+    });
+  });
+
+})
+
+
+//绑定地址直接参与
+$('#participatingWithInviter').on('click', function() {
+    console.log('>>>','participatingWithInviter');
+    var parentAddress = $("#parentAddress").val();
+    var participatingWithInviterIndex = $("#participatingWithInviterIndex").val();
+    var usdtAmount = participatingAmount[participatingWithInviterIndex];
+    var inUsdt = web3.utils.toWei(usdtAmount, "ether");
+    usdtContract.methods.approve(raisingAddress, inUsdt).send({ from: accounts }).then((approveHash) => {
+      raisingContract.methods.participatingWithInviter(_Index, parentAddress).send({from: accounts}, function (error, transactionHash) {
+        console.log("participatingWithInviter>>>>>", error, transactionHash);
+      });
+  });
+})
+
+//领取代币
+$('#divieToken').on('click', function() {
+  console.log('>>>','divieToken');
+  raisingContract.methods.divieToken().send({from: accounts}, function (error, transactionHash) {
+    console.log("divieToken>>>>>", error, transactionHash);
+  });
+})
+
+
+
+//设置代币地址
+$('#setTOKEN').on('click', function() {
+  console.log('>>>','setTOKEN');
+  var tokenAddress = $("#tokenAddress").val();
+  raisingContract.methods.setTOKEN(tokenAddress).send({from: accounts}, function (error, transactionHash) {
+    console.log("setTOKEN>>>>>", error, transactionHash);
+  });
+})
+
+
+//提取剩余usdt
+$('#emergencyWithdrawUsdt').on('click', function() {
+  console.log('>>>','emergencyWithdrawUsdt');
+  var toAddress = $("#toAddress").val();
+  var usetAmount = $("#usetAmount").val();
+  raisingContract.methods.emergencyWithdrawUsdt(toAddress,web3.utils.toWei(usetAmount, "ether")).send({from: accounts}, function (error, transactionHash) {
+    console.log("emergencyWithdrawUsdt>>>>>", error, transactionHash);
+  });
+})
+
+
+//提取剩余代币
+$('#emergencyWithdrawToken').on('click', function() {
+  console.log('>>>','emergencyWithdrawToken');
+  var toTokenAddress = $("#toTokenAddress").val();
+  var tokenAmount = $("#tokenAmount").val();
+  raisingContract.methods.emergencyWithdrawToken(toTokenAddress, web3.utils.toWei(tokenAmount, "ether")).send({from: accounts}, function (error, transactionHash) {
+    console.log("emergencyWithdrawToken>>>>>", error, transactionHash);
+  });
+})
+
+
+
+
+const initialize = () => {
+
+    ethereum.on('accountsChanged', handleAccountsChanged);
+    ethereum.on('chainChanged', handleAccountsChanged);
+
+    async function handleAccountsChanged() {
+        window.location.reload();
+    }
+}
+window.addEventListener('DOMContentLoaded', initialize)
---
+++ lafDao/raisingAbi.js
@@ -0,0 +1,631 @@
+raisingABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_rootAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "parent",
+          "type": "address"
+        }
+      ],
+      "name": "BindReferral",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "on",
+          "type": "bool"
+        }
+      ],
+      "name": "OperatorUpdated",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        }
+      ],
+      "name": "Participatingd",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "TOKEN",
+      "outputs": [
+        {
+          "internalType": "contract IERC20",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "awardsToken",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "awardsTokenOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "balances",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_referral",
+          "type": "address"
+        }
+      ],
+      "name": "bindReferral",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "divieToken",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "emergencyWithdrawToken",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "emergencyWithdrawUsdt",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "name": "getReferral",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "name": "getReferralCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_depth",
+          "type": "uint256"
+        }
+      ],
+      "name": "getReferrals",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_user",
+          "type": "address"
+        }
+      ],
+      "name": "getReferralsList",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getRootAddress",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "name": "isBindReferral",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_account",
+          "type": "address"
+        }
+      ],
+      "name": "isOperator",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        }
+      ],
+      "name": "isPreacher",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_participatingIndex",
+          "type": "uint8"
+        }
+      ],
+      "name": "participating",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "participatingAccountAmount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_participatingIndex",
+          "type": "uint8"
+        },
+        {
+          "internalType": "address",
+          "name": "parent",
+          "type": "address"
+        }
+      ],
+      "name": "participatingWithInviter",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_operator",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "on",
+          "type": "bool"
+        }
+      ],
+      "name": "setOperator",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_token",
+          "type": "address"
+        }
+      ],
+      "name": "setTOKEN",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "t_supply",
+      "outputs": [
+        {
+          "internalType": "uint40",
+          "name": "participatingTime",
+          "type": "uint40"
+        },
+        {
+          "internalType": "uint160",
+          "name": "tamount",
+          "type": "uint160"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "teamTotalParticipationValue",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "teamVirtuallyParticipationValue",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupplyMax",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "userParticipatingRecord",
+      "outputs": [
+        {
+          "internalType": "uint40",
+          "name": "participatingTime",
+          "type": "uint40"
+        },
+        {
+          "internalType": "uint160",
+          "name": "amount",
+          "type": "uint160"
+        },
+        {
+          "internalType": "bool",
+          "name": "status",
+          "type": "bool"
+        },
+        {
+          "internalType": "uint8",
+          "name": "participatingIndex",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
